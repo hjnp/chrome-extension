6683e7c2d9addc5075f735c8418446acdbe6d6b1
hp <hp_ctt@hotmail.com>
2019-05-07 11:04:14 +0100

    wip

---
+++ background.js
@@ -0,0 +1,24 @@
+// Copyright 2018 @hjnp. All rights reserved.
+
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.sync.set({
+        color: '#3aa757'
+    }, function () {
+        console.log("The color is green.");
+    });
+
+
+    chrome.declarativeContent.onPageChanged.removeRules(function () {
+        console.log("onPageChanged.removeRules");
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: {
+                    hostEquals: 'developer.chrome.com'
+                },
+            })],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
+});
